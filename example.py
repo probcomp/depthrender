569f0474b0a8a5cb71deaa21905a235dbcc602c0
Marco Cusumano-Towner <imarcoam@gmail.com>
2021-02-07 12:36:07 -0500

    improve the example script

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from depthrender.render import Renderer
+import tempfile
 
 width = 64
 height = 48
@@ -41,11 +42,11 @@ for i in range(n):
     depth_image = renderer.render(vertices, triangles)
     depth_images.append(depth_image)
 fps = n / (time.time() - start)
-print("%d fps" % (fps,))
+print("rendered %d depth images at %d fps" % (n,fps,))
 
 # do it again, with new parameters
-width = 64
-height = 48
+width = 640
+height = 480
 fx = width/3.0
 fy = width/3.0
 cx = width/2.0
@@ -53,18 +54,20 @@ cy = height/2.0
 renderer.set_camera_params(width, height, fx, fy, cx, cy)
 period = 20.0
 start = time.time()
-n = 10
-for i in range(n):
+for i in range(n, n+n):
     angle = i*2*3.1415926/period
     vertices = np.matmul(original_vertices, np.transpose(R(angle)))
     depth_image = renderer.render(vertices, triangles)
     depth_images.append(depth_image)
 fps = n / (time.time() - start)
-print("%d fps" % (fps,))
+print("rendered %d depth images at %d fps" % (n,fps,))
 
+# save all the images
+print("writing depth images to disk")
+tmpdir = tempfile.gettempdir()
 for (i, img) in enumerate(depth_images):
     print(i)
     plt.figure()
     plt.imshow(img)
-    plt.savefig("output/depth-%03d.png" % (i,))
+    plt.savefig("%s/depth-%03d.png" % (tmpdir,i))
     plt.clf()
